56c6d112d05e90275ac368a3974e98d3e541c1a4
poojan456 <pooja1713953@gmail.com>
2026-08-22 02:30:41 +0530

    Expose user on Order model to allow frontend to render customer emails

--- Backend/Backend/src/main/java/com/Demo_Dmart/Backend/model/Order.java
+++ Backend/Backend/src/main/java/com/Demo_Dmart/Backend/model/Order.java
@@ -14,7 +14,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonIgnore
+    @com.fasterxml.jackson.annotation.JsonIgnoreProperties({"password"})
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
